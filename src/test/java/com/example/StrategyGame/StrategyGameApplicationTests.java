0b7b9ccd42c7ba4e783fdf9381b8113f26d21d91
alexa <alexabianca091@gmail.com>
2024-10-14 13:49:29 +0300

    Setup

---
+++ src/test/java/com/example/StrategyGame/StrategyGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StrategyGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrategyGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
