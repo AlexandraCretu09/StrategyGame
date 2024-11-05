955726555b0db1c2e31872681494852e27149f73
alexa <alexabianca091@gmail.com>
2024-11-05 11:09:23 +0200

    Connected Spring to Postman and the concurrent part

--- CEBP/StrategyGame/src/main/java/com/example/StrategyGame/User/UserService.java
+++ CEBP/StrategyGame/src/main/java/com/example/StrategyGame/User/UserService.java
@@ -1,13 +1,32 @@
 package com.example.StrategyGame.User;
 
+import com.example.StrategyGame.CommandRequest.CommandRequest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
 public class UserService {
     private static final String ConcurrentProjectURL = "http://localhost:8081/api/commands";
 
     public void processUserCommand(User user) {
         RestTemplate restTemplate = new RestTemplate();
-        restTemplate.postForObject(ConcurrentProjectURL, user, String.class);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        CommandRequest commandRequest = new CommandRequest(user.getUsername(), user.getCommand());
+        HttpEntity<CommandRequest> request = new HttpEntity<>(commandRequest, headers);
+
+        ResponseEntity<String> response = restTemplate.postForEntity(ConcurrentProjectURL, request, String.class);
+
+        System.out.println("Response from Java project: " + response.getBody());
+
     }
 
 }
